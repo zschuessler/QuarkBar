4a590c3c52220d11abe12654a6ecfa4972e64533
zac439 <zschuessler@deltasys.com>
2012-03-12 18:37:30 -0500

    show quark bar properly in admin/frontend

--- app/code/community/Zaclee/QuarkBar/Model/Observer.php
+++ app/code/community/Zaclee/QuarkBar/Model/Observer.php
@@ -2,13 +2,60 @@
 
 class Zaclee_QuarkBar_Model_Observer
 {
+
+    protected $_isLoaded;
+
     public function controller_action_predispatch($event)
     {
-        $controller = $event->getController();
+        $request = Mage::app()->getRequest();
         
-        echo '
-            <div class="quarkBar">test</div>
-            <div class="quarkBar-clear">&nbsp;</div>
-';
+        // Don't show a toolbar for ajax requests
+        if ($request->isAjax()) {
+            return;
+        }
+
+        // Show QuarkBar
+        echo '<div class="quarkBar">';
+
+        if ($this->_isAdminModule()) {
+            $this->_showAdminBar();
+        } else {
+            $this->_showFrontendBar();
+        }
+
+        echo '</div>';
+
+        echo '<div class="clear">&nbsp;</div>';
     }
+
+    /**
+     * Checks if the current request is from the admin module.
+     * 
+     * @return bool 
+     */
+    public function _isAdminModule()
+    {
+        if ('admin' == Mage::app()->getRequest()->getModuleName()) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Shows the QuarkBar for administrator module 
+     */
+    public function _showAdminBar()
+    {
+        echo 'admin';
+    }
+
+    /**
+     * Shows the QuarkBar for frontend store 
+     */
+    public function _showFrontendBar()
+    {
+        echo 'frontend';
+    }
+
 }
