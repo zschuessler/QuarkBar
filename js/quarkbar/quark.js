e14b0f98607b4eb46adad333949fa2be4a347092
zac439 <zschuessler@deltasys.com>
2012-03-25 10:43:09 -0500

    add js/css left out in prior commit(s)

--- js/quarkbar/quark.js
+++ js/quarkbar/quark.js
@@ -1,10 +1,53 @@
+
+/**
+ *  Clear the Magento cache
+ */
 jQuery(document).ready(function() {
     jQuery('#quark-clear-cache').click(function() {
-       jQuery.ajax({
-         url: '/quarkbar/ajax/clearCache',
-         success: function(data) {
-             
-         }
-       });
+        var status = jQuery('#quark-nav-status');
+
+        jQuery.ajax({
+            url: '/quarkbar/ajax/clearCache',
+            dataType: 'json',
+            success: function(data) {
+                if( data.status == 1 ) {
+                    status.html(data.message);
+                    status.show('fast').fadeOut(3000);
+                } else {
+                    status.show('fast');
+                }
+            },
+            error: function(data, textStatus) {
+                status.html(textStatus);
+                status.show('fast');
+            }
+        });
+    });
+});
+
+
+/**
+ *  Rebuild Magento Indexes
+ */
+jQuery(document).ready(function() {
+    jQuery('#quark-rebuild-indexes').click(function() {
+        var status = jQuery('#quark-nav-status');
+
+        jQuery.ajax({
+            url: '/quarkbar/ajax/rebuildIndexes',
+            dataType: 'json',
+            success: function(data) {
+                if( data.status == 1 ) {
+                    status.html(data.message);
+                    status.show('fast').fadeOut(3000);
+                } else {
+                    status.show('fast');
+                }
+            },
+            error: function(data, textStatus) {
+                status.html(textStatus);
+                status.show('fast');
+            }
+        });
     });
 });
