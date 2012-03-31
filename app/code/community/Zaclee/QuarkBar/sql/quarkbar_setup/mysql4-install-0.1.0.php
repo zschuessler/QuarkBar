5fb9c66d5cddc2b5020cf2bf8f845d46f68bf2a4
Zachary Schuessler <zlschuessler@gmail.com>
2012-03-31 14:01:14 -0500

    add SQL installer for quarkbar_sessions

--- app/code/community/Zaclee/QuarkBar/sql/quarkbar_setup/mysql4-install-0.1.0.php
+++ app/code/community/Zaclee/QuarkBar/sql/quarkbar_setup/mysql4-install-0.1.0.php
@@ -5,43 +5,17 @@ $installer->startSetup();
 
 // DELETE FROM core_resource WHERE code = 'quarkbar_setup';
 
-$installer->run("
-    DELETE FROM customer_group WHERE customer_group_code = 'admin';
-    TRUNCATE customer_entity;"
-);
-
 /**
- *  Create admin customer group 
+ * Create quarkbar_session to store admin sessions 
  */
-// Find the first tax class for customers so
-//  we can use it in creating the customer group.
-$taxClasses = Mage::getModel('tax/class')->getCollection();
-$taxClasses->addFieldToFilter('class_type', array(
-    'like' => '%CUSTOMER%'
-));
+$installer->run("
+CREATE TABLE IF NOT EXISTS `quarkbar_session` (
+  `quarkbar_session_id` int(11) NOT NULL AUTO_INCREMENT,
+  `quarkbar_user` varchar(255) NOT NULL,
+  `quarkbar_salt` varchar(255) NOT NULL,
+  PRIMARY KEY (`quarkbar_session_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;
+");
 
-$taxClass   = $taxClasses->getFirstItem();
-$taxClassId = $taxClass->getId();
 
-// Add a new customer group 'admin'
-$customerGroup = Mage::getModel('customer/group');
-$customerGroup->setData(array(
-    'customer_group_code' => 'admin',
-    'tax_class_id'        => $taxClassId
-));
-$customerGroup->save();
-
-// Duplicate admin user as a customer
-$admins = Mage::getModel('admin/user')->getCollection();
-
-foreach ($admins as $admin) {
-    $customer = Mage::getModel('customer/customer');
-
-    $customer->setWebsiteId(Mage::app()->getWebsite()->getId())
-            ->setEmail($admin->getEmail())
-            ->setFirstname($admin->getFirstname())
-            ->setLastname($admin->getLastname())
-            ->setPassword($admin->getPassword());
-
-    $customer->save();
-}
+$installer->endSetup();
